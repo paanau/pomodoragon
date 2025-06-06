c4312a2dcf82c35a3ddfb0994430611bb8587208
paanau <alexander.solan@gmail.com>
2025-06-06 16:25:27 +0300

    Testing mode is live, baybee.

--- next.config.js
+++ next.config.js
@@ -4,7 +4,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  basePath: process.env.NODE_ENV === 'production' ? '/pomodoragon' : '',
+  basePath: '/pomodoragon',
 }
 
 module.exports = nextConfig 
